f61835e8ced83dd2166296342a228aaeb5b93417
kbbushman <k.bushman@yahoo.com>
2018-11-19 08:10:32 -0800

    Adds cors

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const PORT = process.env.PORT || 5000;
 
+// Initialize App
 const app = express();
+
+// Allow Cross-Origin Requests
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
+
+// Start Server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));

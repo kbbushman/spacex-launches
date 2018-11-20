b68e643864cc1c9f96f100c381e54ab9c5cd6273
kbbushman <k.bushman@yahoo.com>
2018-11-19 17:59:53 -0800

    Adds heroku deploy config

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 // Initialize App
 const app = express();
@@ -15,6 +16,11 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
+app.use(express.static('public'));
+
+app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 // Start Server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));

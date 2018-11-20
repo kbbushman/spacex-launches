70138ac1aa85b248c0e6a7a1f2b9e861ad5a8d2c
kbbushman <k.bushman@yahoo.com>
2018-11-19 18:30:31 -0800

    Adds public directory for heroku deploy

---
+++ public/precache-manifest.e3d63238f215afb3e9f872630036fa4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9e23481c22d230774eb",
+    "url": "/static/js/main.e9e23481.chunk.js"
+  },
+  {
+    "revision": "638c271555c9281efe32",
+    "url": "/static/js/1.638c2715.chunk.js"
+  },
+  {
+    "revision": "e9e23481c22d230774eb",
+    "url": "/static/css/main.8bc04114.chunk.css"
+  },
+  {
+    "revision": "2ac95e8824095b13c7ad0bf44ecb1d80",
+    "url": "/index.html"
+  }
+];
